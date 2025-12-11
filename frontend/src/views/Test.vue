76b1d68a5e3e931b5d5ba12477d701cf287384ea
Rossi <1142595193@qq.com>
2025-12-11 17:45:22 +0800

    移除没用文件

--- frontend/src/views/Test.vue
+++
@@ -1,8 +0,0 @@
-<!-- App.vue -->
-<template>
-  <Presence room-id="lobby" user-id="user-456" user-name="张三" avatar="/avatars/zhangsan.jpg" />
-</template>
-
-<script setup lang="ts">
-import Presence from '@/components/Presence.vue'
-</script>
